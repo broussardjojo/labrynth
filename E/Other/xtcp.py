1e8cabfb830c26cdfc943c5281e1c8cc1dc32e33
Thomas Mactaggart <thomasmactaggart@Thomass-MacBook-Pro.local>
2022-09-26 18:28:55 -0400

    do assignment E

---
+++ E/Other/xtcp.py
@@ -0,0 +1,51 @@
+import socket
+import sys
+import subprocess
+from json import JSONDecoder
+
+
+def parse_data(data_in):
+    output_list = []
+    json_obj_list = []
+    # remove leading spaces
+    stdin = data_in.lstrip()
+    # initialize decoder object
+    decoder = JSONDecoder()
+    # loop through standard input reading JSON
+    while len(stdin) > 0:
+        json_obj, index = decoder.raw_decode(stdin)
+        json_obj_list.append(json_obj)
+        # remove previously read object from string
+        stdin = stdin[index:]
+        stdin = stdin.lstrip()
+    # convert json objects to characters
+    for i in json_obj_list:
+        if i["vertical"] == "UP":
+            if i["horizontal"] == "RIGHT":
+                output_list.append("└")
+            else:
+                output_list.append("┘")
+        else:
+            if i["horizontal"] == "RIGHT":
+                output_list.append("┌")
+            else:
+                output_list.append("┐")
+    return output_list
+
+
+if __name__ == "__main__":
+    port = int(sys.argv[1])
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', port))
+    s.listen()
+    conn, addr = s.accept()
+    output = []
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            output = output + parse_data(data.decode())
+            if not data:
+                conn.sendall((str(output) + '\n').encode())
+                break
+        conn.close()
+    s.close()
